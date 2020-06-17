1caa8eecb38e874ea5e95459bfc83e34d09e6dd9
ThomasCarroll <tc.infomatics@gmail.com>
2020-06-17 15:30:15 -0400

    Add condaTools functions

--- R/functions.R
+++ R/functions.R
@@ -63,3 +63,51 @@ install_CondaSysReqs <- function(pkg,channels=NULL,pathToMiniConda=NULL,updateEn
   return(list(pathToConda=pathToConda,environment=environment))
 }
 
+
+#' Install Conda requirements.
+#'
+#' Install Conda requirements
+#'
+#'
+#' @name install_CondaTools
+#' @rdname install_CondaTools
+#'
+#'
+#' @author Thomas Carroll
+#' @param tools software to install using conda.
+#' @param env Conda environment to install tools into.
+#' @param channels Additional channels for miniconda (bioconda defaults and conda-forge are included automatically)
+#' @param pathToMiniConda NULL Path to miniconda installation
+#' @param updateEnv Update existing package's conda environment if already installed.
+#' @return Nothing returned. Output written to file.
+#' @import utils reticulate rjson
+#' @export
+install_CondaTools <- function(tools,env,channels=NULL,pathToMiniConda=NULL,updateEnv=FALSE){
+  # pathToMiniConda <- "~/Desktop/testConda"
+  
+  if(is.null(pathToMiniConda)) pathToMiniConda <- reticulate::miniconda_path()
+  
+
+  pathToCondaInstall <- pathToMiniConda
+  pathToConda <- file.path(pathToCondaInstall,"bin","conda")
+  
+  
+  defaultChannels <-  c("bioconda","defaults","conda-forge")
+  channels <- unique(c(channels,defaultChannels))
+  environment <- env
+  pathToMiniCondaPkgEnv <- file.path(pathToMiniConda,"envs",environment)
+  
+  miniCondaPathExists <- miniconda_exists(pathToMiniConda)
+  miniCondaPkgEnvPathExists <- dir.exists(pathToMiniCondaPkgEnv)
+  
+  if(!miniCondaPathExists) reticulate::install_miniconda(pathToCondaInstall)
+  if(!miniCondaPkgEnvPathExists) reticulate::conda_create(envname=environment,conda=pathToConda)
+  if(!miniCondaPkgEnvPathExists | (miniCondaPkgEnvPathExists & updateEnv)){
+    reticulate::conda_install(envname = environment,packages = tools,
+                              conda=pathToConda,
+                              channel = channels)
+  }
+  return(list(pathToConda=pathToConda,environment=environment))
+}
+
+
