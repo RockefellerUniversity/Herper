2a586299cc895a8893824e3dffc08013ceb9a651
matthew-paul-2006 <matthew.paul.2006@gmail.com>
2020-09-25 16:10:52 -0400

    format updates

--- R/list_CondaPkgs.R
+++ R/list_CondaPkgs.R
@@ -11,49 +11,56 @@
 #' @param env environment to look in
 #' @param pathToMiniConda NULL Path to miniconda installation
 #' @param pkg Package name. If this is supplied to list_CondaPkg, it will query whether that package is present in the given environment.
-#' @return Conda package information is printed to the screen. If package name is supplied a TRUE/FALSE will be returned depending on whether that package is present or not. 
+#' @return Conda package information is printed to the screen. If package name is supplied a TRUE/FALSE will be returned depending on whether that package is present or not.
 #' @import reticulate rjson
-#' @examples 
-#' condaDir <- file.path(tempdir(),'r-miniconda')
-#' condaPaths <- install_CondaTools('salmon','herper',pathToMiniConda=condaDir)
-#' list_CondaPkgs('herper', condaDir)
+#' @examples
+#' condaDir <- file.path(tempdir(), "r-miniconda")
+#' condaPaths <- install_CondaTools("salmon", "herper", pathToMiniConda = condaDir)
+#' list_CondaPkgs("herper", condaDir)
 #' @export
-list_CondaPkgs <- function(env, pathToMiniConda = NULL, 
+list_CondaPkgs <- function(env, pathToMiniConda = NULL,
                            pkg = NULL) {
   # pathToMiniConda <- '~/my_miniconda/' env='herper'
-  
+
   if (!is.null(pathToMiniConda)) {
     pathToMiniConda <- normalizePath(pathToMiniConda)
     pathToMiniConda <- file.path(pathToMiniConda)
   } else {
     pathToMiniConda <- reticulate::miniconda_path()
   }
-  
-  pathToConda <- file.path(pathToMiniConda, "bin", 
-                           "conda")
-  
+
+  pathToConda <- file.path(
+    pathToMiniConda, "bin",
+    "conda"
+  )
+
   args <- paste0("-n", env)
-  
-  result <- suppressWarnings(system2(pathToConda, 
-                                     shQuote(c("list", args, "--quiet", "--json")), 
-                                     stdout = TRUE, stderr = TRUE))
+
+  result <- suppressWarnings(system2(pathToConda,
+    shQuote(c("list", args, "--quiet", "--json")),
+    stdout = TRUE, stderr = TRUE
+  ))
   result <- rjson::fromJSON(paste(result, collapse = ""))
-  
+
   if ("exception_name" %in% names(result)) {
     if (result$exception_name == "EnvironmentLocationNotFound") {
-      message(paste0("The environment ", env, 
-                     " was not found./n Use list_CondaEnv() to check what environments are available."))
+      message(paste0(
+        "The environment ", env,
+        " was not found./n Use list_CondaEnv() to check what environments are available."
+      ))
     } else {
       message(message("Unexepected conda error. conda command failed."))
     }
   }
-  
+
   result <- do.call(rbind.data.frame, result)
   rownames(result) <- NULL
-  
+
   if (!is.null(pkg)) {
     return(any(result[, "name"] %in% pkg))
   }
-  print(result[, c("name", "version", "channel", 
-                   "platform")])
+  print(result[, c(
+    "name", "version", "channel",
+    "platform"
+  )])
 }
