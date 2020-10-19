19e5e3225ab0fd1bac6e7508f50ae336bdf3544f
matthew-paul-2006 <matthew.paul.2006@gmail.com>
2020-10-19 13:29:03 -0400

    added a mock test

--- tests/testthat/test-herper.R
+++ tests/testthat/test-herper.R
@@ -9,11 +9,23 @@ myMiniconda <- file.path(tempdir2(),"Test")
 test_that("install_CondaTools", {
   expect_equal(length(install_CondaTools("multiqc", "herper", pathToMiniConda = myMiniconda)), 3)
   if(!identical(.Platform$OS.type, "windows")){expect_error(install_CondaTools("multi", "herper", pathToMiniConda = myMiniconda))
-    }else{expect_message(install_CondaTools("multi", "herper", pathToMiniConda = myMiniconda))}
+  }else{expect_message(install_CondaTools("multi", "herper", pathToMiniConda = myMiniconda))}
 })
+
+#Test install with mock
+test_that("install_CondaTools_mock", {
+  mockr::with_mock(
+    conda_install_silentJSON = function(...) {},
+    {expect_equal(names(install_CondaTools("multiqc", "herper2", pathToMiniConda = myMiniconda)), c("pathToConda","environment","pathToEnvBin"))})})
 
 #Test that installed functionality works
 test_that("with_CondaEnv", {
   expect_equal(grepl("version", with_CondaEnv("herper", system2("multiqc","--version", stdout = TRUE),
                                                        pathToMiniConda = myMiniconda)), TRUE)
 })
+
+#Conda search testing
+test_that("conda_search", {
+  expect_equal(conda_search("multiqc",pathToMiniConda=myMiniconda),T)
+})
+
