9f395246588162f20a313c57b5adf65c76e1860a
matthew-paul-2006 <matthew.paul.2006@gmail.com>
2022-10-22 22:38:08 -0400

    update env for tests

--- tests/testthat/test-herper.R
+++ tests/testthat/test-herper.R
@@ -8,7 +8,7 @@ myMiniconda <- file.path(tempdir2(),"Test")
 #Test that the install works okay
 test_that("1_install_CondaTools", {
   expect_equal(length(install_CondaTools("multiqc", "herper", pathToMiniConda = myMiniconda)), 3)
-  if(!identical(.Platform$OS.type, "windows")){expect_error(install_CondaTools("multi", "herper", pathToMiniConda = myMiniconda))
+  if(!identical(.Platform$OS.type, "windows")){expect_error(install_CondaTools("mui", "herper", pathToMiniConda = myMiniconda, updateEnv = T))
   }else{expect_message(install_CondaTools("multi", "herper", pathToMiniConda = myMiniconda))}
 })
 
